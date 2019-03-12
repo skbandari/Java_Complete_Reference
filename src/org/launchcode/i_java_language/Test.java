74a8055851693675e01749187c78f50d9d012f48
skbandari <34947517+skbandari@users.noreply.github.com>
2019-03-12 21:11:08 +0000

    Update Test.java

--- src/org/launchcode/i_java_language/Test.java
+++ src/org/launchcode/i_java_language/Test.java
@@ -2,6 +2,6 @@ package org.launchcode.i_java_language;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World Test");
     }
 }
